33cbb6878c1183aa7166665eef18a255addb984b
aalvarado <adan.alvarado7@gmail.com>
2018-06-15 19:48:25 -0700

    Setup elements

--- src/imgurp.js
+++ src/imgurp.js
@@ -4,8 +4,21 @@ const HEADER_ELEMENT = '.post-header [data-reactroot]';
 const REACT_FUNCTION = 0;
 const REACT_ELEMENT = 0;
 const DEFAULT_TRANSTION_TIME = 5000;
+const SLIDE_SHOW_ID = 'slide-show';
+const BODY_STYLE = 'overflow: hidden;';
+const SLIDE_SHOW_STYLE = `
+  display: block;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 102;
+  background-color: black;
+`
 
 const qsa = document.querySelectorAll.bind(document);
+const ce = document.createElement.bind(document);
 
 const getReactComponent = (selector) => {
   let el = qsa(selector).item(REACT_ELEMENT).wrappedJSObject;
@@ -19,5 +32,15 @@ const getReactComponent = (selector) => {
 const getHeader = () => {
   return getReactComponent(HEADER_ELEMENT);
 }
+
+const setSlideShowElement = () => {
+  let div = ce('div')
+  div.setAttribute('id', SLIDE_SHOW_ID);
+  div.setAttribute('style', SLIDE_SHOW_STYLE);
+  return document.body.appendChild(div);
+}
+
+setSlideShowElement();
+document.body.setAttribute('style', BODY_STYLE);
 
 //setInterval(() => (getHeader()._next()), DEFAULT_TRANSTION_TIME);
